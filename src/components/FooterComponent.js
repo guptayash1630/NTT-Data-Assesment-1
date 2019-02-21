017f25874bc0b664a1c3e7f5de55114bc3f3a5de
guptayash1630 <47625210+guptayash1630@users.noreply.github.com>
2019-02-21 22:01:40 +0530

    Add files via upload
    Added missing footer component

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FooterComponent extends React.Component {
+    render() {
+        return (           
+            <div className="header">                
+                <h1>© 2019 Copyright:</h1>
+            </div>
+        );
+    }
+};
+
+export default FooterComponent;
+
